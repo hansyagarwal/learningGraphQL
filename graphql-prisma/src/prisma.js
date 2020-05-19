6f3c27fc9a950e8f65ec62ec90053bb3389d7e73
hansyagarwal <hansy.agarwal@gmail.com>
2020-05-20 02:36:45 +0530

    preping up for authentication

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,54 +5,56 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466'
 })
 
+export {prisma as default}
+
 //prisma.query prisma.mutation prisma.subscription prisma.exists
 
-const createPostForUser = async (authorId, data)=>{
+// const createPostForUser = async (authorId, data)=>{
 
-    const userExists = await prisma.exists.User({id: authorId})
+//     const userExists = await prisma.exists.User({id: authorId})
 
-    if(!userExists) {
-        throw new Error('User not found')
-    }
+//     if(!userExists) {
+//         throw new Error('User not found')
+//     }
 
-    const post = await prisma.mutation.createPost({
-        data:{
-            ...data,
-            author: {
-                connect: {
-                    id: authorId
-                }
-            }
-        }
-    }, '{ author{ id name email posts {id title published } } }')
-    return post.author
-}
+//     const post = await prisma.mutation.createPost({
+//         data:{
+//             ...data,
+//             author: {
+//                 connect: {
+//                     id: authorId
+//                 }
+//             }
+//         }
+//     }, '{ author{ id name email posts {id title published } } }')
+//     return post.author
+// }
 
-// createPostForUser('cka5qy9ps00do0836zfyzhjx2',{
-//     title: 'Greate books to read',
-//     body: 'The Art of war',
-//     published: true
-// }).then((user)=>{
-//     console.log(JSON.stringify(user, undefined, 2))
-// }).catch((error)=>{
-//     console.log(error.message)
-// })
+// // createPostForUser('cka5qy9ps00do0836zfyzhjx2',{
+// //     title: 'Greate books to read',
+// //     body: 'The Art of war',
+// //     published: true
+// // }).then((user)=>{
+// //     console.log(JSON.stringify(user, undefined, 2))
+// // }).catch((error)=>{
+// //     console.log(error.message)
+// // })
 
-const updatePostForUser = async (postId, data)=>{
-    const postExists = await prisma.exists.Post({id: postId})
+// const updatePostForUser = async (postId, data)=>{
+//     const postExists = await prisma.exists.Post({id: postId})
 
-    if(!postExists) {
-        throw new Error('Post not found')
-    }
+//     if(!postExists) {
+//         throw new Error('Post not found')
+//     }
 
-    const post = await prisma.mutation.updatePost({
-        data,
-        where:{
-            id: postId
-        }
-    }, '{ author{ id name email posts { id title published } } }')
-    return post.author
-}
+//     const post = await prisma.mutation.updatePost({
+//         data,
+//         where:{
+//             id: postId
+//         }
+//     }, '{ author{ id name email posts { id title published } } }')
+//     return post.author
+// }
 
 // updatePostForUser('cka9ubdgm00n50836vbrtoip2',{
 //     published: false
--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,24 +1,39 @@
 const Query = {
-    users(parent, args, {db}, info) {
-        if(!args.query) {
-            return db.users
+    users(parent, args, {prisma}, info) {
+        const opArgs = {}
+
+        if(args.query){
+            opArgs.where = {
+                OR: [{
+                    name_contains: args.query
+                }, {
+                    email_contains: args.query
+                }]
+            }
         }
-        return db.users.filter((user)=>{
-            return user.name.toLowerCase().includes(args.query.toLowerCase())
-        })
+
+        return prisma.query.users(opArgs, info)
     },
-    posts(parent, args, {db}, info) {
-        if(!args.search) {
-            return db.posts
+    posts(parent, args, {prisma}, info) {
+ 
+        const opArgs = {}
+
+        if(args.search){
+            opArgs.where = {
+                OR: [{
+                    title_contains: args.search
+                }, {
+                    body_contains: args.search
+                }]
+            }
         }
-        return db.posts.filter((post)=>{
-            const isTitle = post.title.toLowerCase().includes(args.search.toLowerCase())
-            const isBody = post.body.toLowerCase().includes(args.search.toLowerCase())
-            return isTitle || isBody
-        })
+
+        return prisma.query.posts(opArgs,info)
+
     },
-    comments(parent,args,{db},info) {
-        return db.comments
+    comments(parent,args,{prisma},info) {
+
+        return prisma.query.comments(null, info)
     },
     me() {
         return {

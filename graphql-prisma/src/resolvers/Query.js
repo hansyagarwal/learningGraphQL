a18badb2a71be129c8797960d41d2057376556b3
hansyagarwal <hansy.agarwal@gmail.com>
2020-05-30 23:52:28 +0530

    sorting

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -5,7 +5,8 @@ const Query = {
         const opArgs = {
             first: args.first,
             skip: args.skip,
-            after: args.after
+            after: args.after,
+            orderBy: args.orderBy
         }
 
         if(args.query){
@@ -47,6 +48,7 @@ const Query = {
             first: args.first,
             skip: args.skip,
             after: args.after,
+            orderBy: args.orderBy,
             where: {
                 published: true
             }
@@ -67,7 +69,8 @@ const Query = {
         const opArgs = { 
             first: args.first,
             skip: args.skip,
-            after: args.after  
+            after: args.after,
+            orderBy: args.orderBy
         }
         return prisma.query.comments(opArgs, info)
     },
